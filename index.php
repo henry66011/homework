acb3b2bdbe55e9a64035bcbfc8c0e48260deb814
henry66011 <100329053+henry66011@users.noreply.github.com>
2022-02-25 03:46:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+
+<html lang="en">
+<head>
+	<title>testPHP</title>
+</head>
+<body>
+	<?php
+	$testVariable="helloWorld" ;
+	$testNumber=200;
+	echo "testString</br>";
+	echo "<h2>$testVariable</h2></br>";
+	echo "$testNumber";
+	 ?>
+</body>
+</html>
